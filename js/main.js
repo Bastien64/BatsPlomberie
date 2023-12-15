95dd8e53dd43a8e86fb117e4b5d6d51e73639a3a
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-12-15 20:25:56 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Charger la navbar
+var navbarContainer = document.getElementById("navbarbox");
+var navbarXhr = new XMLHttpRequest();
+navbarXhr.open("GET", "../htmlelement/navbar.html", true);
+navbarXhr.onreadystatechange = function () {
+    if (navbarXhr.readyState === 4 && navbarXhr.status === 200) {
+        navbarContainer.innerHTML = navbarXhr.responseText;
+    }
+};
+navbarXhr.send();
+
+// Charger le footer
+var footerContainer = document.getElementById("footerbox");
+var footerXhr = new XMLHttpRequest();
+footerXhr.open("GET", "../htmlelement/footer.html", true);
+footerXhr.onreadystatechange = function () {
+    if (footerXhr.readyState === 4 && footerXhr.status === 200) {
+        footerContainer.innerHTML = footerXhr.responseText;
+    }
+};
+footerXhr.send();
+
+// Charger le divbleutop
+var divbleutopContainer = document.getElementById("divbleutopbox");
+var divbleutopXhr = new XMLHttpRequest();
+divbleutopXhr.open("GET", "../htmlelement/divbleutop.html", true);
+divbleutopXhr.onreadystatechange = function () {
+    if (divbleutopXhr.readyState === 4 && divbleutopXhr.status === 200) {
+        divbleutopContainer.innerHTML = divbleutopXhr.responseText;
+    }
+};
+divbleutopXhr.send();
